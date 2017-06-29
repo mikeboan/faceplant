7a15337b1510a578a1197461400fc0a7771c3d3d
Mike Boan <mikeboan@gmail.com>
2017-06-29 16:05:48 -0400

    add cover photo and adjust user routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170629014627) do
+ActiveRecord::Schema.define(version: 20170629192248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,10 @@ ActiveRecord::Schema.define(version: 20170629014627) do
     t.string   "profile_pic_content_type"
     t.integer  "profile_pic_file_size"
     t.datetime "profile_pic_updated_at"
+    t.string   "cover_photo_file_name"
+    t.string   "cover_photo_content_type"
+    t.integer  "cover_photo_file_size"
+    t.datetime "cover_photo_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
