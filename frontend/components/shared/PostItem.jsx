23b446c8ac4d13563bfcc0779a4d14991a47a4a9
Mike Boan <mikeboan@gmail.com>
2017-07-10 13:01:27 -0400

    add non-functional edit/delete buttons to post

--- frontend/components/shared/PostItem.jsx
+++ frontend/components/shared/PostItem.jsx
@@ -8,31 +8,49 @@ const mapStateToProps = (state, ownProps) => {
   const { user_id } = ownProps.post;
 
   return {
-    user: selectUser(state, user_id)
+    user: selectUser(state, user_id),
+    currentUser: state.session.currentUser
   };
 };
 
-const PostItem = ({ post, user }) => {
-  const timeAgo = moment(post.created_at, "YYYYMMDD").calendar();
+class PostItem extends React.Component {
 
-  return (
-  <li className='post-item'>
-    <div className='post-header'>
-      <a href={`profiles/${user.id}`}>
-        <img
-          className='profile-pic-thumbnail'
-          src={user.profilePicUrl}
-          />
-      </a>
-      <div className='metadata'>
-        <a href={`profiles/${user.id}`}>{ user.name }</a>
-        <time>{ timeAgo }</time>
-      </div>
-    </div>
-    <div className='post-contents'>
-      { post.content }
-    </div>
-  </li>
-)};
+  editNav() {
+    return (
+      <nav>
+        <i className="fa fa-pencil" aria-hidden="true"></i>
+        <i className="fa fa-trash" aria-hidden="true"></i>
+      </nav>
+    );
+  }
+
+  render() {
+    const { post, user, currentUser } = this.props;
+    const timeAgo = moment(post.created_at, "YYYYMMDD").calendar();
+
+    return (
+      <li className='post-item'>
+        <div className='post-header'>
+          <div className='info'>
+            <a href={`profiles/${user.id}`}>
+              <img
+                className='profile-pic-thumbnail'
+                src={user.profilePicUrl}
+                />
+            </a>
+            <div className='metadata'>
+              <a href={`profiles/${user.id}`}>{ user.name }</a>
+              <time>{ timeAgo }</time>
+            </div>
+          </div>
+          { user.id === currentUser.id ? this.editNav() : "" }
+        </div>
+        <div className='post-contents'>
+          { post.content }
+        </div>
+      </li>
+    )
+  }
+}
 
 export default connect(mapStateToProps)(PostItem);
