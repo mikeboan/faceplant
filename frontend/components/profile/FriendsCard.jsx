85cf96e14db76e4f19a848605ccb0ab606f54137
Mike Boan <mikeboan@gmail.com>
2017-07-23 19:43:12 -0400

    Factor out generic profile card

--- frontend/components/profile/FriendsCard.jsx
+++ frontend/components/profile/FriendsCard.jsx
@@ -2,24 +2,29 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { selectFriends } from '../../selectors/selectors';
+import GenericCard from './GenericCard';
 import FriendListItem from './FriendListItem';
 
 const mapStateToProps = (state, { user }) => ({
-  friends: selectFriends(state, user)
+  cardName: "Friends",
+  cardIcon: window.friendsBubble,
+  listItems: selectFriends(state, user).map(
+    friend => <FriendListItem key={ friend.id } friend={friend} />
+  ),
 });
 
-const FriendsCard = ({ user, friends }) => (
-  <section className='friends-card card'>
-    <header>
-      <img src={ window.friendsBubble }></img>
-      <h2>Friends</h2>
-    </header>
-    <ul>
-      {
-        friends.map( friend => <FriendListItem key={ friend.id } friend={friend} /> )
-      }
-    </ul>
-  </section>
-);
+// const FriendsCard = ({ user, friends }) => (
+//   <section className='friends-card card'>
+//     <header>
+//       <img src={ window.friendsBubble }></img>
+//       <h2>Friends</h2>
+//     </header>
+//     <ul>
+//       {
+//         friends
+//       }
+//     </ul>
+//   </section>
+// );
 
-export default connect(mapStateToProps)(FriendsCard);
+export default connect(mapStateToProps)(GenericCard);
---
+++ frontend/components/profile/GenericCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const GenericCard = ({ cardIcon, cardName, listItems }) => (
+  <section className={ `${cardName.toLowerCase()}-card card` }>
+    <header>
+      <img src={ cardIcon }></img>
+      <h2>{ cardName }</h2>
+    </header>
+    <ul>
+      { listItems }
+    </ul>
+  </section>
+);
+
+export default GenericCard;
