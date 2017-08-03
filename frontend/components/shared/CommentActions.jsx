af11db22dbdfb843b7c9827a384269322abd4181
Mike Boan <mikeboan@gmail.com>
2017-08-03 15:39:50 -0400

    conditionally show like/unlike text

--- frontend/components/shared/CommentActions.jsx
+++ frontend/components/shared/CommentActions.jsx
@@ -6,29 +6,23 @@ import { postLike, deleteLike } from '../../redux/modules/likes';
 
 const mapStateToProps = (state, { commentId }) => ({
   likers: selectCommentLikers(state, commentId),
-  currentUserLikesComment: currentUserLikesComment(state, commentId),
+  likedByCurrentUser: currentUserLikesComment(state, commentId),
 });
 
-const mapDispatchToProps = (dispatch, { commentId, currentUserLikesComment }) => {
-  let clickAction = postLike;
-  let likeText = "Like";
+const mapDispatchToProps = (dispatch, { commentId, likedByCurrentUser }) => ({
+  postLike: () => dispatch(postLike(commentId, "Comment")),
+  deleteLike: () => dispatch(deleteLike(commentId, "Comment")),
+});
 
-  if (currentUserLikesComment) {
-    clickAction = deleteLike;
-    likeText = "Unlike";
-  }
+const CommentActions = ({ likers, likedByCurrentUser, postLike, deleteLike }) => {
+  const clickAction = likedByCurrentUser ? deleteLike : postLike;
+  const likeText = likedByCurrentUser ? "Unlike" : "Like";
 
-  return {
-    clickAction: () => dispatch(clickAction(commentId, "Comment")),
-    likeText
-  };
-};
-
-const CommentActions = ({ clickAction, likeText, likers }) => (
+  return (
   <div
     className='comment-actions'>
-    <span onClick={ postLike }>{ likeText }</span> Reply Likes: { likers.length }
+    <span onClick={ clickAction }>{ likeText }</span> Reply Likes: { likers.length }
   </div>
-);
+)};
 
 export default connect(mapStateToProps, mapDispatchToProps)(CommentActions);
