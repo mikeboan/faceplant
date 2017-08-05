c556cd6f0b80ac71ffd384f6a529cd75caecee6b
Mike Boan <mikeboan@gmail.com>
2017-08-05 11:45:03 -0400

    Remove unused helpers

--- frontend/redux/modules/helpers.js
+++ frontend/redux/modules/helpers.js
@@ -2,35 +2,5 @@ export const arrayify = (objsById) => (
   Object.keys(objsById).map(id => objsById[id])
 );
 
-export const normalize = (obj, targetKey) => {
-  if (!isObject(obj) || isEmpty(obj)) return {};
-
-  const items = {};
-
-  Object.keys(obj).forEach( key => {
-    if (key === targetKey) Object.assign(items, obj[key]);
-    else Object.assign(items, normalize(obj[key], targetKey));
-  });
-
-  return Object.assign(items, normalize(items, targetKey));
-};
-
-export const normalize2 = (obj, targetKey) => {
-  if (!isObject(obj) || isEmpty(obj)) return {};
-
-  const items = {};
-
-  Object.keys(obj).forEach( key => {
-    const item = obj[key];
-    if (key === targetKey) Object.assign(items, { [item.id]: item });
-    else Object.assign(items, normalize(item, targetKey));
-  });
-
-
-  return {};
-};
-
-window.normalize2 = normalize2;
-
 export const isObject = (item) => item !== null && typeof item === 'object';
 export const isEmpty = (obj) => Object.keys(obj).length === 0;
