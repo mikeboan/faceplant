ffb566675d47b66e6e87bc1857b95d48cd8f3c33
Mike Boan <mikeboan@gmail.com>
2017-08-30 15:52:49 -0400

    Remove posts from timeline

--- frontend/redux/modules/profiles.js
+++ frontend/redux/modules/profiles.js
@@ -27,24 +27,33 @@ const api = {
   }),
 };
 
-window.profileApi = api;
-window.profileActions = Object.assign(syncActions, { fetchProfile });
-
 // profile reducer
 const profilesByUserId = (oldState = {}, action) => {
+  const newState = Object.assign({}, oldState);
+
   switch(action.type) {
     case RECEIVE_PROFILE:
       return Object.assign({}, oldState, action.entities.profiles);
 
     case RECEIVE_POST:
       const { posts } = action.entities;
-      const newState = Object.assign({}, oldState);
       // add post ids to appropriate profile timelines
       Object.keys(posts).forEach(postId => {
         const post = posts[postId];
         const profile = newState[post.profileUserId];
         profile.timelinePosts = [postId, ...profile.timelinePosts];
       });
+      return newState;
+
+    case REMOVE_POST:
+      Object.keys(action.entities.posts).map( id => action.entities.posts[id] )
+        .forEach( post => {
+          const profile = newState[post.profileUserId];
+          profile.timelinePosts = profile.timelinePosts.filter((postId) => (
+            parseInt(postId) !== action.result
+          ));
+        }
+      );
       return newState;
 
     default:
