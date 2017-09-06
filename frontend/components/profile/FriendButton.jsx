404a7692b7195dcda9aa05e757878d427a09432e
Mike Boan <mikeboan@gmail.com>
2017-09-05 21:49:35 -0400

    Add friend request drop down

--- frontend/components/profile/FriendButton.jsx
+++ frontend/components/profile/FriendButton.jsx
@@ -1,25 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { friendStatusWithCurrentUser } from '../../selectors/selectors.js';
+import { friendStatusWithCurrentUser, dropdownOpen } from '../../selectors/selectors.js';
 import {
   postFriendship,
   destroyFriendship,
   updateFriendship
 } from '../../redux/modules/users';
+import { showDropdown, hideDropdown } from '../../redux/modules/dropdowns';
 
 const mapStateToProps = (state, { currentUser, user }) => {
   const propsByStatus = {
     none: { disabled: false, buttonText: "Add Friend"},
     accepted: { disabled: false, buttonText: "Remove Friend"},
     requested: { disabled: true, buttonText: "Pending..."},
-    pending: { disabled: false, buttonText: "Accept Request"},
+    pending: { disabled: false, buttonText: "Respond to Request"},
     rejected: { disabled: false, buttonText: "Add Friend"},
   }
 
   const friendStatus = friendStatusWithCurrentUser(currentUser, user.id);
-
-  return { friendStatus, propsByStatus };
+  console.log(dropdownOpen, showDropdown, hideDropdown);
+  return {
+    friendStatus,
+    propsByStatus,
+    visible: dropdownOpen(state, 'friend-request')
+  };
 }
 
 const mapDispatchToProps = (dispatch, { currentUser, user }) => ({
@@ -27,19 +32,57 @@ const mapDispatchToProps = (dispatch, { currentUser, user }) => ({
     none: () => dispatch(postFriendship(user.id)),
     accepted: () => dispatch(destroyFriendship(user.id)),
     requested: () => ({}),
-    pending: () => dispatch(updateFriendship(user.id, 'accepted')),
+    pending: status => dispatch(updateFriendship(user.id, status)),
     rejected: () => dispatch(postFriendship(user.id)),
-  }
+  },
+  showDropdown: () => dispatch(showDropdown('friend-request')),
+  hideDropdown: () => dispatch(hideDropdown('friend-request')),
 });
 
-const FriendButton = ({ friendStatus, propsByStatus, actionsByStatus }) => (
-  <button
-    className="friend-button"
-    onClick={ (e) => { e.preventDefault; actionsByStatus[friendStatus](); } }
-    disabled={ propsByStatus[friendStatus].disabled }
-  >
-    { propsByStatus[friendStatus].buttonText }
-  </button>
-);
+const FriendButton = ({ friendStatus, propsByStatus, actionsByStatus, visible, showDropdown, hideDropdown }) => {
+
+  if (friendStatus === 'pending') {
+    const clickAction = e => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      if (visible) hideDropdown();
+      else showDropdown();
+    }
+
+    return (
+      <button
+        className="friend-button"
+        onClick={ clickAction }
+        disabled={ propsByStatus[friendStatus].disabled }
+      >
+        { propsByStatus[friendStatus].buttonText }
+        <ul className={ visible ? "friend-request-dd shadow" : "hidden" }>
+          <li onClick={ () => actionsByStatus[friendStatus]('accepted') }>
+            Accept Request
+          </li>
+          <li onClick={ () => actionsByStatus[friendStatus]('rejected') }>
+            Reject Request
+          </li>
+        </ul>
+      </button>
+    );
+  } else {
+    return (
+      <button
+        className="friend-button"
+        onClick={ (e) => { e.preventDefault; actionsByStatus[friendStatus](); } }
+        disabled={ propsByStatus[friendStatus].disabled }
+      >
+        { propsByStatus[friendStatus].buttonText }
+
+      </button>
+    );
+  }
+};
+
+// const FriendButton = ({ friendStatus, propsByStatus, actionsByStatus }) => (
+//
+// );
 
 export default connect(mapStateToProps, mapDispatchToProps)(FriendButton);
